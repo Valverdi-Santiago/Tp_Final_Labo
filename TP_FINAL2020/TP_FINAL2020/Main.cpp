ce0db9c81d3835e4819b7a689da8dac0a541d1cc
clain <clain@DESKTOP-4L9IPTU>
2020-11-06 11:50:11 -0300

    Puse las clases logger y lista, y comente en el main lo que pense

---
+++ TP_FINAL2020/TP_FINAL2020/Main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include "cAvion.h"
+#include "cCodigoPasaje.h"
+#include "cPersona.h"
+
+
+
+int main()
+{
+
+	//Crear un avion
+	//Crear una lista de Personas con sus codigos
+	//Agregar y verificar las pasajeros y tripulantes al avion, mediante el codigo (la lista de codigos habilitada para viajar la tiene el avion)
+	//Hacer un simulador, donde ocurren distintos eventos, los cuales se alamecan en el logger
+	//Mostrar en pantalla todos los eventos que pasaron
+
+
+
+	system("pause");
+	return 0;
+}
